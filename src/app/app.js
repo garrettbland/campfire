69969c22f7dc69eab46ff2184ccdc41536cb3650
garrettbland <gmorganbland@gmail.com>
2020-05-17 14:21:56 -0500

    updated comments

--- src/app/app.js
+++ src/app/app.js
@@ -3,6 +3,12 @@ import addSectionEditButtons from './components/addSectionEditButtons'
 import sectionTools from './components/sectionTools'
 const app = () => {
     console.log('🚀 stating app...')
+
+    /**
+     * Map out tools and utilities, and initialize
+     * Accessed from view layer with app.{}.{}
+     * For example, 'app.section.editBackground'
+     */
     return {
         section: {
             ...sectionTools,
