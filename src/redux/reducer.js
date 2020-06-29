768af1a61f6d10743a86ab511fe19be0103f5edc
garrettbland <gmorganbland@gmail.com>
2020-06-28 22:46:06 -0500

    setup reach router

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,7 +4,78 @@ import { INCREMENT, DECREMENT } from './constants'
  * Define initial state for campfire app
  */
 let initialState = {
-    count: 0,
+    website: {
+        head: {
+            title: 'Campfire site',
+            description: 'This is a website',
+            scripts: ['google gtag analytics', 'google fonts'],
+        },
+        data: [
+            {
+                id: '39xs',
+                type: 'body',
+                classes: ['bg-gray-100'],
+                data: [
+                    {
+                        id: '129r',
+                        type: 'div',
+                        classes: [
+                            'flex',
+                            'flex-wrap',
+                            'items-center',
+                        ],
+                        data: [
+                            {
+                                id: 'xx82',
+                                type: 'div',
+                                classes: ['w-full', 'md:w-1/2'],
+                                data: [
+                                    {
+                                        id: '1x93',
+                                        type: 'h1',
+                                        classes: [
+                                            'text-4xl',
+                                            'font-bold',
+                                        ],
+                                        data: {
+                                            text:
+                                                'Welcome to campfire',
+                                        },
+                                    },
+                                    {
+                                        id: '393x',
+                                        type: 'p',
+                                        classes: ['text-gray-800'],
+                                        data: {
+                                            text:
+                                                'This is a website builder',
+                                        },
+                                    },
+                                ],
+                            },
+                            {
+                                id: 'xv82',
+                                type: 'div',
+                                classes: ['w-full', 'md:w-1/2'],
+                                data: [
+                                    {
+                                        id: '39z1',
+                                        type: 'img',
+                                        classes: [],
+                                        data: {
+                                            src:
+                                                'https://images.unsplash.com/photo-1593059315234-fe82cafe19e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+                                            alt: 'Example image',
+                                        },
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
 }
 
 /**
--- src/router.js
+++ src/router.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import { Router } from '@reach/router'
 
 /**
  * Views
  */
 import Home from './views/Home'
+import NotFound from './views/NotFound'
 
-const Router = () => {
+const Navigation = () => {
     return (
-        <>
-            <Home />
-        </>
+        <Router>
+            <Home path="/" />
+            <NotFound default />
+        </Router>
     )
 }
 
-export default Router
+export default Navigation
