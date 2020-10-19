953a8c9e12c3065e41533f0a28b8c242392cd1fc
Garrett Bland <gmorganbland@gmail.com>
2020-10-19 09:52:30 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './app'
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./app";
 
-const app = document.getElementById('campfire_app')
-ReactDom.render(<App />, app)
+const app = document.getElementById("campfire_app");
+ReactDom.render(<App />, app);
