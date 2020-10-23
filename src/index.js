3821cab88a3ed160309e5d4af12c06766f43ff9f
garrettbland <gmorganbland@gmail.com>
2020-10-23 10:33:48 -0500

    still cleaning up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import Router from './router'
-import './style/styles.css'
+import App from './app'
 
 const rootElement = document.getElementById('app')
-ReactDOM.render(
-    <Provider store={store}>
-        <Router />
-    </Provider>,
-    rootElement
-)
+ReactDOM.render(<App />, rootElement)
