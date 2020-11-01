1ab6ed9e89283929aaa2c8a8165e753d90a3deca
garrettbland <gmorganbland@gmail.com>
2020-11-01 09:20:15 -0600

    moved text content into own component

---
+++ src/components/TextContent.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { UPDATE_EDITING } from '../redux/constants'
+import { ReactTrixRTEInput } from 'react-trix-rte'
+
+const TextContent = () => {
+    const currentlyEditing = useSelector((state) => state.currentlyEditing)
+    const dispatch = useDispatch()
+
+    const handleTextChange = (event, newValue) => {
+        dispatch({
+            type: UPDATE_EDITING,
+            payload: {
+                ...currentlyEditing,
+                data: newValue,
+            },
+        })
+    }
+
+    return (
+        <div>
+            <ReactTrixRTEInput defaultValue={currentlyEditing.data} onChange={handleTextChange} />
+        </div>
+    )
+}
+
+export default TextContent
--- src/components/TextEdit.js
+++ src/components/TextEdit.js
@@ -1,25 +1,15 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { UPDATE_EDITING, REMOVE_BLOCK } from '../redux/constants'
-import { ReactTrixRTEInput } from 'react-trix-rte'
 import LineHeight from './LineHeight'
 import FontSize from './FontSize'
 import TextAlignment from './TextAlignment'
 import TextColor from './TextColor'
+import TextContent from './TextContent'
 
 const TextEdit = () => {
     const currentlyEditing = useSelector((state) => state.currentlyEditing)
     const dispatch = useDispatch()
-
-    const handleTextChange = (event, newValue) => {
-        dispatch({
-            type: UPDATE_EDITING,
-            payload: {
-                ...currentlyEditing,
-                data: newValue,
-            },
-        })
-    }
 
     const RemoveBlock = () => {
         dispatch({
@@ -34,7 +24,8 @@ const TextEdit = () => {
 
     return (
         <div>
-            <ReactTrixRTEInput defaultValue={currentlyEditing.data} onChange={handleTextChange} />
+            <TextContent />
+            {/* <ReactTrixRTEInput defaultValue={currentlyEditing.data} onChange={handleTextChange} /> */}
             <div>
                 <button onClick={() => RemoveBlock()}>Remove Text</button>
             </div>
