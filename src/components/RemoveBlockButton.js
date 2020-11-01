cf26eb33af1a3f8ac6167a0b4382e8863f98060c
garrettbland <gmorganbland@gmail.com>
2020-11-01 09:24:43 -0600

    moved remove block button into component

---
+++ src/components/RemoveBlockButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { UPDATE_EDITING, REMOVE_BLOCK } from '../redux/constants'
+
+const RemoveBlockButton = ({ title = 'Remove Element' }) => {
+    const currentlyEditing = useSelector((state) => state.currentlyEditing)
+    const dispatch = useDispatch()
+
+    const RemoveBlock = () => {
+        dispatch({
+            type: REMOVE_BLOCK,
+            payload: currentlyEditing,
+        })
+
+        dispatch({
+            type: UPDATE_EDITING,
+        })
+    }
+
+    return (
+        <div>
+            <button onClick={() => RemoveBlock()}>{title}</button>
+        </div>
+    )
+}
+
+export default RemoveBlockButton
--- src/components/TextEdit.js
+++ src/components/TextEdit.js
@@ -1,38 +1,20 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { UPDATE_EDITING, REMOVE_BLOCK } from '../redux/constants'
-import LineHeight from './LineHeight'
-import FontSize from './FontSize'
-import TextAlignment from './TextAlignment'
-import TextColor from './TextColor'
 import TextContent from './TextContent'
+import TextColor from './TextColor'
+import TextAlignment from './TextAlignment'
+import FontSize from './FontSize'
+import LineHeight from './LineHeight'
+import RemoveBlockButton from './RemoveBlockButton'
 
 const TextEdit = () => {
-    const currentlyEditing = useSelector((state) => state.currentlyEditing)
-    const dispatch = useDispatch()
-
-    const RemoveBlock = () => {
-        dispatch({
-            type: REMOVE_BLOCK,
-            payload: currentlyEditing,
-        })
-
-        dispatch({
-            type: UPDATE_EDITING,
-        })
-    }
-
     return (
         <div>
             <TextContent />
-            {/* <ReactTrixRTEInput defaultValue={currentlyEditing.data} onChange={handleTextChange} /> */}
-            <div>
-                <button onClick={() => RemoveBlock()}>Remove Text</button>
-            </div>
             <TextColor />
             <TextAlignment />
             <FontSize />
             <LineHeight />
+            <RemoveBlockButton title="Remove Text" />
         </div>
     )
 }
