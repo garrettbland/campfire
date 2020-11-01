131a828c425034b14b105a9d704e25b125b0df8d
garrettbland <gmorganbland@gmail.com>
2020-11-01 09:14:15 -0600

    moved text color into own component

---
+++ src/components/TextColor.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { UPDATE_EDITING } from '../redux/constants'
+import { extractClass } from '../utils/tools'
+import { generateColors, removeTextColorClasses } from '../utils/colors'
+
+const TextColor = () => {
+    const currentlyEditing = useSelector((state) => state.currentlyEditing)
+    const [textColor, setTextColor] = useState('')
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const currentTextColor = extractClass(currentlyEditing.classList, generateColors('text'))
+        if (currentTextColor) {
+            setTextColor(currentTextColor)
+        }
+    }, [currentlyEditing.id])
+
+    const handleTextColorUpdate = (value) => {
+        setTextColor(value)
+
+        /**
+         * Filter out any text color classes
+         */
+        const updatedClassList = removeTextColorClasses(currentlyEditing.classList)
+
+        dispatch({
+            type: UPDATE_EDITING,
+            payload: {
+                ...currentlyEditing,
+                classList: [...updatedClassList, value],
+            },
+        })
+    }
+
+    return (
+        <div>
+            <div>Text Color: {textColor ? textColor : 'N/A'}</div>
+            <div className="flex flex-wrap">
+                {[...generateColors('text'), ''].map((color) => {
+                    return (
+                        <div
+                            key={color}
+                            onClick={() => handleTextColorUpdate(color)}
+                            className={`w-8 h-8 ${
+                                color ? color.replace('text-', 'bg-') : 'bg-teal-500'
+                            } border cursor-pointer`}
+                        >
+                            {color === textColor ? 'Selected' : null}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default TextColor
--- src/components/TextEdit.js
+++ src/components/TextEdit.js
@@ -1,25 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { UPDATE_EDITING, REMOVE_BLOCK } from '../redux/constants'
 import { ReactTrixRTEInput } from 'react-trix-rte'
-import { extractClass } from '../utils/tools'
-import { generateColors, removeTextColorClasses } from '../utils/colors'
 import LineHeight from './LineHeight'
 import FontSize from './FontSize'
 import TextAlignment from './TextAlignment'
+import TextColor from './TextColor'
 
 const TextEdit = () => {
     const currentlyEditing = useSelector((state) => state.currentlyEditing)
-    const [textColor, setTextColor] = useState('')
     const dispatch = useDispatch()
-
-    useEffect(() => {
-        const currentTextColor = extractClass(currentlyEditing.classList, generateColors('text'))
-
-        if (currentTextColor) {
-            setTextColor(currentTextColor)
-        }
-    }, [])
 
     const handleTextChange = (event, newValue) => {
         dispatch({
@@ -42,45 +32,13 @@ const TextEdit = () => {
         })
     }
 
-    const handleTextColorUpdate = (value) => {
-        setTextColor(value)
-
-        /**
-         * Filter out any text color classes
-         */
-        const updatedClassList = removeTextColorClasses(currentlyEditing.classList)
-
-        dispatch({
-            type: UPDATE_EDITING,
-            payload: {
-                ...currentlyEditing,
-                classList: [...updatedClassList, value],
-            },
-        })
-    }
-
     return (
         <div>
             <ReactTrixRTEInput defaultValue={currentlyEditing.data} onChange={handleTextChange} />
             <div>
                 <button onClick={() => RemoveBlock()}>Remove Text</button>
             </div>
-            <div>Tetx Color: {textColor ? textColor : 'N/A'}</div>
-            <div className="flex flex-wrap">
-                {[...generateColors('text'), ''].map((color) => {
-                    return (
-                        <div
-                            key={color}
-                            onClick={() => handleTextColorUpdate(color)}
-                            className={`w-8 h-8 ${
-                                color ? color.replace('text-', 'bg-') : 'bg-teal-500'
-                            } border cursor-pointer`}
-                        >
-                            {color === textColor ? 'Selected' : null}
-                        </div>
-                    )
-                })}
-            </div>
+            <TextColor />
             <TextAlignment />
             <FontSize />
             <LineHeight />
